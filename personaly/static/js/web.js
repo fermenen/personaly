f033d1a91a1d47d3e04b748331fd839d677fcb6d
Fernando Menendez <f.menendez@betterplaceapp.com>
2020-12-02 11:26:38 +0100

    changes

---
+++ personaly/static/js/web.js
@@ -0,0 +1,9 @@
+$(window).on('load resize', function () {
+    var win = $(this);
+    if (win.width() <= 600) {
+        $('#text-2-web').addClass('uk-flex-first')
+    }else{
+        $('#text-2-web').removeClass('uk-flex-first')
+    }
+
+});
